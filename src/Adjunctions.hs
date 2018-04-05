a0ae87037febdad615b13c511758b9c7b4d22564
Jamie WJ <jamie_wj@yahoo.com>
2018-04-05 14:37:27 +0100

    adjunction type class set up

--- src/Adjunctions.hs
+++ src/Adjunctions.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE ExplicitForAll #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
 
 module Adjunctions where
 
@@ -11,11 +13,51 @@ data MyProduct a b = P { first :: a, second :: b }
 
 data MyReader a b = R {runReader :: a -> b }
 
+data MyIdentity a = I { unI :: a }
+
 instance Functor (MyProduct a) where
     fmap f p = P (first p) (f $ second p)
 
 instance Functor (MyReader a) where
     fmap f r = R $ f . runReader r
 
+instance Functor (MyIdentity) where
+    fmap f (I x) = I $ f x
+
+
 type f ~> g = forall x . f x -> g x
+data CompF f g a = CompF { unCompF :: f (g a) }
+
+-- (a -> b) -> f (g a) -> f (g b)
+instance  (Functor f, Functor g) => Functor (CompF f g) where
+    fmap fun c = CompF $ fmap (fmap fun) (unCompF c)
+    
+
+-- F ⊢ G
+class (Functor f, Functor g) => MyAdjunction f g where
+    -- eta
+    unit :: CompF f g ~> MyIdentity
+    -- epsilon
+    counit :: MyIdentity ~> CompF g f
+
+
 -- Need to find natural transformations:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
