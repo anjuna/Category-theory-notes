82702f99180b5cc42a3e9b38a621a34950d19709
Jamie WJ <jamie_wj@yahoo.com>
2018-04-22 19:02:58 +0100

    got Tape comonad working

--- src/comonadLoeb.hs
+++ src/comonadLoeb.hs
@@ -14,25 +14,46 @@ loebNew fs = fix $ \xs -> fmap ($ xs) fs
 -- hmmmm
 
 -- cant load package for some reason, here's the typeclass anyway
-
 class (Functor w) => Comonad w where
     coreturn :: w a -> a 
-    cojoin :: w a -> w (w a)
-    (=>>) :: w a -> (w a -> b) -> w b
-    x =>> f = fmap f $ cojoin x
+    duplicate :: w a -> w (w a)
+    extend :: w a -> (w a -> b) -> w b
+    extend x f = fmap f $ duplicate x
     
 
 
 data Stream a = Cons a (Stream a) deriving Functor
 
+head :: Stream a -> a 
+head (Cons x _) = x 
 
--- eg real number line stretching -Infinity to +Infinity
-data Tape a = Tape (Stream a) a (Stream a) deriving Functor
+tail :: Stream a -> Stream a 
+tail (Cons _ xs) = xs
+
+-- useful for seeing things in repl
+takeN :: Int -> Stream a -> [a]
+takeN n (Cons x xs) = if n == 0 then [] else (x:(takeN (n - 1) xs))
+
+
+-- eg real number line stretching -Infinity to +Infinity with 0 in the middle
+data Tape a = Tape { left :: Stream a, focus :: a, right :: Stream a} deriving Functor
+
+-- move the focus to the left
+moveLeft :: Tape a -> Tape a
+moveLeft (Tape (Cons lh lt) f r) = Tape lt lh (Cons f r)
+
+-- move the focus to the right
+moveRight :: Tape a -> Tape a
+moveRight (Tape l f (Cons rh rt)) = Tape (Cons f l) rh rt
+
+iterStream :: (a -> a) -> a -> Stream a
+iterStream f e = let eAgain = f e 
+                in Cons e (iterStream f eAgain)
+
 
 instance Comonad Tape where
-    coreturn Tape (l x r) = x
-    cojoin 
-
+    coreturn (Tape _ x _) = x
+    duplicate t = Tape (iterStream moveLeft t) t (iterStream moveRight t)
 
 
 
